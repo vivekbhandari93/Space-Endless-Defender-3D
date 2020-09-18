f10f16e2531a0a0ef7cb6fea499a421cabf492dd
Vivek Singh Bhandari <ervivekbhandari@gmail.com>
2020-09-18 16:56:00 +0530

    Splash screen with game sound added, waypoints added.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+        Invoke("LoadFirstScene", 2f);        
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
