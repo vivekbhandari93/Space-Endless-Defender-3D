6749bd63cb2466312fab477d3f1f69afd574a250
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-02 13:14:27 +0530

    Destroying enemies. Instantiate deathVFX, self destroy on deathVFX. Check on Music Player singleton.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
+    private void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject instantVFX = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        instantVFX.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
